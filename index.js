70fb42e402829dc890d243530ceb84b909a13868
collins <kipkorirc583@gmail.com>
2023-07-07 21:25:28 +0300

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Pseudocode:
+  // 1. Create an empty set to store seen numbers.
+  // 2. Iterate through the array.
+  //    - For each number, calculate the complement (target - current number).
+  //    - Check if the complement exists in the set.
+  //    - If it does, return true.
+  //    - If it doesn't, add the current number to the set.
+  // 3. If the loop completes without finding a pair, return false.
+
+  const seen = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (seen.has(complement)) {
+      return true;
+    }
+    seen.add(array[i]);
+  }
+
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Time complexity: O(n), where n is the length of the array.
+  The function iterates through the array once, performing constant-time operations for each element.
+
+  Explanation:
+  The `hasTargetSum` function uses a set to keep track of seen numbers. It iterates through the array, calculating the complement for each number and checking if the complement exists in the set. If the complement is found, it means there is a pair of numbers that sums up to the target. If the loop completes without finding a pair, it means there is no such pair in the array.
 */
 
-/* 
-  Add your pseudocode here
-*/
+// Test Suite
+describe("hasTargetSum", () => {
+  it("returns true when the array is [3, 8, 12, 4, 11, 7] and the target is 10", () => {
+    expect(hasTargetSum([3, 8, 12, 4, 11, 7], 10)).to.be.true;
+  });
 
-/*
-  Add written explanation of your solution here
-*/
+  it("returns true when the array is [22, 19, 4, 6, 30] and the target is 25", () => {
+    expect(hasTargetSum([22, 19, 4, 6, 30], 25)).to.be.true;
+  });
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  it("returns false when the array is [1, 2, 5] and the target is 4", () => {
+    expect(hasTargetSum([1, 2, 5], 4)).to.be.false;
+  });
 
-  console.log("");
+  it("returns true when the array is [-7, 10, 4, 8] and the target is 3", () => {
+    expect(hasTargetSum([-7, 10, 4, 8], 3)).to.be.true;
+  });
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  it("returns true when the array is [1, 2, 3, 4] and the target is 5", () => {
+    expect(hasTargetSum([1, 2, 3, 4], 5)).to.be.true;
+  });
 
-  console.log("");
+  it("returns true when the array is [2, 2, 3, 3] and the target is 4", () => {
+    expect(hasTargetSum([2, 2, 3, 3], 4)).to.be.true;
+  });
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
+  it("returns false when the array is [4] and the target is 4", () => {
+    expect(hasTargetSum([4], 5)).to.be.false;
+  });
+});
